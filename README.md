47cb528cb8f4203accec711269a65f71c5d30f73
steedalot <daniel@gaida.io>
2024-12-03 20:38:11 +0100

    aktuelle Version, lauffähig

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # RundenMeister
 
--> Läuft auch irgendwann! :)
+RundenMeister ist ein einfaches System zur Erfassung von Rundenzeiten bei Laufveranstaltungen, besonders geeignet für Schulveranstaltungen.
+
+## API-Dokumentation
+
+Die API verwendet JSON für die Kommunikation. Alle Anfragen müssen als POST-Requests gesendet werden.
+
+### Runde hinzufügen
+
+*Anfrage*
+
+Um eine Runde hinzuzufügen, senden Sie eine POST-Anfrage an die API mit folgendem JSON-Body:
+
+```json
+{
+    "action": "add_lap",
+    "runner_id": "RUNNER_ID",
+    "type": "lap"
+}
+
+* action: Muss immer "add_lap" sein.
+* runner_id: Die eindeutige ID des Läufers.
+* type: Der Typ der Runde, kann "start", "lap" oder "finish" sein.
+
+*Antwort*
+
+* Bei Erfolg: Die ID der hinzugefügten Runde.
+* Bei Fehler: Eine Fehlermeldung, z.B. "🚨 Die Runde wurde zu früh hinzugefügt.".
+
+### Ergebnisse anzeigen
+
+Um alle erfassten Runden für einen Läufer bzw. eine Läuferin anzuzeigen, nutzen Sie folgende Anfrage:
+
+```json
+{
+    "action": "get_laps",
+    "runner_id": "RUNNER_ID"
+}
---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+require "rb-mysql.php";
+require "config.php";
+
+$header = "Content-Type: application/json;charset=utf-8";
+ini_set('display_errors', 'On');
+
+R::setup('mysql:host='.MYSQL_HOST.';dbname='.MYSQL_DATABASE, MYSQL_USER, MYSQL_PASS);
+
+$json = file_get_contents('php://input');
+$data = json_decode($json);
+
+$answer = NULL;
+$status = NULL;
+
+
+
+if (isset($data->action)) {
+
+    header($header);
+
+    switch ($data->action) {
+
+        case "add_lap":
+
+            $last_lap = R::findOne("lap", "runner_id = '".$data->runner_id."' ORDER BY timestamp DESC LIMIT 1");
+            $lap = R::dispense('lap');
+            // The type should be of "start", "lap", "finish"
+            $lap->type = $data->type;
+            // The id is the unique uuid of each runner
+            $lap->runner_id = $data->runner_id;
+            // The timestamp is the time when the lap was added
+            $lap->timestamp = time();
+            if ($last_lap && $lap->timestamp < ($last_lap->timestamp + MINIMAL_LAP_TIME)) {
+                $answer = "🚨 Die Runde wurde zu früh hinzugefügt.";
+                $status = 400;
+                R::trash($lap);
+                break;
+            }
+            $id = R::store($lap);
+            $answer = $id;
+            $status = 200;
+        
+            break;
+        
+        case "get_laps":
+                
+                $laps = R::find("lap", "runner_id = '".$data->runner_id."' ORDER BY timestamp ASC");
+                $answer = json_encode(R::exportAll($laps));
+                $status = 200;
+    
+                break;
+    }
+}
+else {
+    $answer = "🚨 Keine Aktion angegeben";
+    $status = 400;
+}
+
+R::close();
+
+http_response_code($status);
+echo $answer;
+
+?>
